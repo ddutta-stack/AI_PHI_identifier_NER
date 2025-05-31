bb833aa18e599da96778d0b3e492465d74a49a63
ddutta <dibyendu.dutta08@gmail.com>
2025-05-31 23:29:20 +0530

    Updated new commits for responses

--- NER.py
+++ NER.py
@@ -4,6 +4,7 @@ import gradio as gr
 
 ## Deepseek API url
 ollamaurl = "http://localhost:11434/api/generate"
+
 def extract_phi_entities(text):
     """
     Extract PHI entities from the given text using a pre-trained model.
@@ -15,13 +16,13 @@ def extract_phi_entities(text):
         "stream": False,
     }
     response = requests.post(ollamaurl, json=payload)
-    print(response.status_code, response.text)
     if response.status_code == 200:
         return response.json().get("response", "No entities found.")
     else:
         return f"Error: {response.status_code} - {response.text}"
+    
 ## TEsting the function
-    if __name__ == "__main__":
+if __name__ == "__main__":
         sample_test_text = "John Doe's phone number is 123-456-7890 and his email is abc@xyz.com and his SSN is 123-45-6789."
         print("Testing extract_phi_entities function...")
         result = extract_phi_entities(sample_test_text)
